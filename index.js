a53d81fccb121c985dd4f75a7116d47668c460d9
JaimeFVZ <jamesfvz@gmail.com>
2020-12-30 18:50:44 +0100

    primer commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import FootballLeague from './classes/PointsBasedLeague.js'
+import KnockoutRounds from './classes/KnockoutRounds.js'
+
+const ROUNDS= 4
+const config = { roundsLeague: 1 }
+const nextRoundTeams= new KnockoutRounds;
+
+
+for (let i=0;i<ROUNDS;i++){
+    const nextRound = new FootballLeague('World Cup', nextRoundTeams.listNextRoundTeams, config) 
+    //const teamNames = nextRound.teams.map(team => team.name)
+
+    nextRound.scheduleMatchDays()
+// Mostramos por pantala las jornadas y sus partidos
+    nextRoundTeams.showTeamsKnockout(nextRound.matchDaySchedule)
+// Comenzamos la liga
+    nextRound.start()
+// mostrar por pantalla los resultados de cada jornada y la clasificación
+    nextRoundTeams.resultsMatchKnockout(nextRound.summaries)
+
+}
+
+console.log (' --------GANADOR---------')
+console.log (nextRoundTeams.listNextRoundTeams[0])
+
+
+
+
