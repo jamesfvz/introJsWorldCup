8830e6f42dac5d6f91a89eacc4331dc4296b8278
JaimeFVZ <jamesfvz@gmail.com>
2021-01-04 13:01:39 +0100

    pruebo a subir un solo archivo en el commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import FootballLeague from './classes/PointsBasedLeague.js'
+import FootballLeague from './classes/Matches.js'
 import KnockoutRounds from './classes/KnockoutRounds.js'
 
 const ROUNDS= 4
@@ -8,7 +8,6 @@ const nextRoundTeams= new KnockoutRounds;
 
 for (let i=0;i<ROUNDS;i++){
     const nextRound = new FootballLeague('World Cup', nextRoundTeams.listNextRoundTeams, config) 
-    //const teamNames = nextRound.teams.map(team => team.name)
 
     nextRound.scheduleMatchDays()
 // Mostramos por pantala las jornadas y sus partidos
